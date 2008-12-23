8730c7c83eb4a7f6f28e1f0845999162f404216e
Matt Oakes <oakesm9@googlemail.com>
2008-12-23 22:48:03 +0000

    Added tag.getWeeklyArtistsChart example

---
+++ examples/tag.getweeklyartistchart/index.php
@@ -0,0 +1,37 @@
+<?php
+
+// Include the API
+require '../../lastfmapi/lastfmapi.php';
+
+// Get the session auth data
+$file = fopen('../auth.txt', 'r');
+// Put the auth data into an array
+$authVars = array(
+	'apiKey' => trim(fgets($file)),
+	'secret' => trim(fgets($file)),
+	'tagname' => trim(fgets($file)),
+	'sessionKey' => trim(fgets($file)),
+	'subscriber' => trim(fgets($file))
+);
+// Pass the array to the auth class to eturn a valid auth
+$auth = new lastfmApiAuth('setsession', $authVars);
+
+$apiClass = new lastfmApi();
+$tagClass = $apiClass->getPackage($auth, 'tag');
+
+// Setup the variables
+$methodVars = array(
+	'tag' => 'emo'
+);
+
+if ( $chart = $tagClass->getWeeklyArtistChart($methodVars) ) {
+	echo '<b>Data Returned</b>';
+	echo '<pre>';
+	print_r($chart);
+	echo '</pre>';
+}
+else {
+	die('<b>Error '.$tagClass->error['code'].' - </b><i>'.$tagClass->error['desc'].'</i>');
+}
+
+?>
