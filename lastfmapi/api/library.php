8a81bce0da5697c222b508ece11fe1c9a1538c54
Matt Oakes <oakesm9@googlemail.com>
2008-12-24 00:32:16 +0000

    Added library.addtrack method

--- lastfmapi/api/library.php
+++ lastfmapi/api/library.php
@@ -71,7 +71,42 @@ class lastfmApiLibrary extends lastfmApiBase {
 			}
 			else {
 				// Give a 91 error if incorrect variables are used
-				$this->handleError(91, 'You must include artist and album varialbes in the call for this method');
+				$this->handleError(91, 'You must include artist varialbe in the call for this method');
+				return FALSE;
+			}
+		}
+		else {
+			// Give a 92 error if not fully authed
+			$this->handleError(92, 'Method requires full auth. Call auth.getSession using lastfmApiAuth class');
+			return FALSE;
+		}
+	}
+	
+	public function addTrack($methodVars) {
+		// Only allow full authed calls
+		if ( $this->fullAuth == TRUE ) {
+			// Check for required variables
+			if ( !empty($methodVars['artist']) && !empty($methodVars['track']) ) {
+				$vars = array(
+					'method' => 'library.addtrack',
+					'api_key' => $this->auth->apiKey,
+					'artist' => $methodVars['artist'],
+					'track' => $methodVars['track'],
+					'sk' => $this->auth->sessionKey
+				);
+				$sig = $this->apiSig($this->auth->secret, $vars);
+				$vars['api_sig'] = $sig;
+				
+				if ( $call = $this->apiPostCall($vars) ) {
+					return TRUE;
+				}
+				else {
+					return FALSE;
+				}
+			}
+			else {
+				// Give a 91 error if incorrect variables are used
+				$this->handleError(91, 'You must include artist and track varialbes in the call for this method');
 				return FALSE;
 			}
 		}
