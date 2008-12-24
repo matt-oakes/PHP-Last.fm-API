f0daa3244d924d2cb0d6aaa8bd090955adb38c1c
Matt Oakes <oakesm9@googlemail.com>
2008-12-24 00:05:31 +0000

    Added playlist.create method

--- lastfmapi/api/playlist.php
+++ lastfmapi/api/playlist.php
@@ -47,6 +47,50 @@ class lastfmApiPlaylist extends lastfmApiBase {
 		}
 	}
 	
+	public function create($methodVars) {
+		// Only allow full authed calls
+		if ( $this->fullAuth == TRUE ) {
+			$vars = array(
+				'method' => 'playlist.create',
+				'api_key' => $this->auth->apiKey,
+				'sk' => $this->auth->sessionKey
+			);
+			if ( !empty($methodVars['title']) ) {
+				$vars['title'] = $methodVars['title'];
+			}
+			if ( !empty($methodVars['description']) ) {
+				$vars['description'] = $methodVars['description'];
+			}
+			$sig = $this->apiSig($this->auth->secret, $vars);
+			$vars['api_sig'] = $sig;
+			
+			if ( $call = $this->apiPostCall($vars, 'xml') ) {
+				$playlist['user'] = (string) $call->playlists['user'];
+				$playlist['id'] = (string) $call->playlists->playlist->id;
+				$playlist['title'] = (string) $call->playlists->playlist->title;
+				$playlist['description'] = (string) $call->playlists->playlist->description;
+				$playlist['date'] = strtotime(trim((string) $call->playlists->playlist->date));;
+				$playlist['size'] = (string) $call->playlists->playlist->size;
+				$playlist['duration'] = (string) $call->playlists->playlist->duration;
+				$playlist['creator'] = (string) $call->playlists->playlist->creator;
+				$playlist['url'] = (string) $call->playlists->playlist->url;
+				$playlist['image']['small'] = (string) $call->playlists->playlist->image[0];
+				$playlist['image']['medium'] = (string) $call->playlists->playlist->image[1];
+				$playlist['image']['large'] = (string) $call->playlists->playlist->image[2];
+				
+				return $playlist;
+			}
+			else {
+				return FALSE;
+			}
+		}
+		else {
+			// Give a 92 error if not fully authed
+			$this->handleError(92, 'Method requires full auth. Call auth.getSession using lastfmApiAuth class');
+			return FALSE;
+		}
+	}
+	
 	public function fetch($methodVars) {
 		// Check for required variables
 		if ( !empty($methodVars['playlistUrl']) ) {
