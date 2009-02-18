6445c284281556aa75ed269dc7f6058858d27fd2
Matt Oakes <oakesm9@googlemail.com>
2009-02-18 17:32:05 +0000

    Split some methods to avoid repeating

--- lastfmapi/class/apibase.php
+++ lastfmapi/class/apibase.php
@@ -3,30 +3,25 @@
 class lastfmApiBase {
 	public $error;
 	
+	private $host;
+	private $port;
+	private $url;
+	private $response;
 	private $socket;
 	private $cache;
 	private $config;
 	
-	function apiGetCall($vars) {
-		$host = 'ws.audioscrobbler.com';
-		$port = 80;
+	function setup() {
+		$this->host = 'ws.audioscrobbler.com';
+		$this->port = 80;
 		
-		$url = '/2.0/?';
-		foreach ( $vars as $name => $value ) {
-			$url .= trim(urlencode($name)).'='.trim(urlencode($value)).'&';
-		}
-		$url = substr($url, 0, -1);
-		$url = str_replace(' ', '%20', $url);
-		
-		$this->socket = new lastfmApiSocket($host, $port);
-		$out = "GET ".$url." HTTP/1.0\r\n";
-   		$out .= "Host: ".$host."\r\n";
-   		$out .= "\r\n";
-		$response = $this->socket->send($out, 'array');
-		
+		$this->socket = new lastfmApiSocket($this->host, $this->port);
+	}
+	
+	function process_response() {
 		$xmlstr = '';
 		$record = 0;
-		foreach ( $response as $line ) {
+		foreach ( $this->response as $line ) {
 			if ( $record == 1 ) {
 				$xmlstr .= $line;
 			}
@@ -53,9 +48,26 @@ class lastfmApiBase {
 		}
 	}
 	
+	function apiGetCall($vars) {
+		$this->setup();
+		
+		$url = '/2.0/?';
+		foreach ( $vars as $name => $value ) {
+			$url .= trim(urlencode($name)).'='.trim(urlencode($value)).'&';
+		}
+		$url = substr($url, 0, -1);
+		$url = str_replace(' ', '%20', $url);
+		
+		$out = "GET ".$url." HTTP/1.0\r\n";
+   		$out .= "Host: ".$this->host."\r\n";
+   		$out .= "\r\n";
+		$this->response = $this->socket->send($out, 'array');
+		
+		return $this->process_response();
+	}
+	
 	function apiPostCall($vars, $return = 'bool') {
-		$host = 'ws.audioscrobbler.com';
-		$port = 80;
+		$this->setup();
 		
 		$url = '/2.0/';
 		
@@ -69,46 +81,14 @@ class lastfmApiBase {
 		$this->socket = new lastfmApiSocket($host, $port);
 		
 		$out = "POST ".$url." HTTP/1.1\r\n";
-   		$out .= "Host: ".$host."\r\n";
+   		$out .= "Host: ".$this->host."\r\n";
    		$out .= "Content-Length: ".strlen($data)."\r\n";
    		$out .= "Content-Type: application/x-www-form-urlencoded\r\n";
    		$out .= "\r\n";
    		$out .= $data."\r\n";
-		
 		$response = $this->socket->send($out, 'array');
 		
-		$xlstr = '';
-		$record = 0;
-		foreach ( $response as $line ) {
-			if ( $record == 1 ) {
-				$xmlstr .= $line;
-			}
-			elseif( substr($line, 0, 1) == '<' ) {
-				$record = 1;
-			}
-		}
-		
-		$xml = new SimpleXMLElement($xmlstr);
-		
-		if ( $xml['status'] == 'ok' ) {
-			if ( $return == 'bool' ) {
-				// All is well :)
-				return TRUE;
-			}
-			else if ($return == 'xml' ) {
-				return $xml;
-			}
-		}
-		elseif ( $xml['status'] == 'failed' ) {
-			// Woops - error has been returned
-			$this->handleError($xml->error);
-			return FALSE;
-		}
-		else {
-			// I put this in just in case but this really shouldn't happen. Pays to be safe
-			$this->handleError();
-			return FALSE;
-		}
+		return $this->process_response();
 	}
 	
 	function handleError($error = '', $customDesc = '') {
