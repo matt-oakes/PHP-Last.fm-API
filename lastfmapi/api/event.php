260d873a516bb5623994f362b340c9f1bc9f5879
Matt Oakes <oakesm9@googlemail.com>
2009-04-02 22:41:24 +0000

    Added method and example for artist.shout

--- lastfmapi/api/event.php
+++ lastfmapi/api/event.php
@@ -205,6 +205,41 @@ class lastfmApiEvent extends lastfmApiBase {
 			return FALSE;
 		}
 	}
+	
+	public function shout($methodVars) {
+		// Only allow full authed calls
+		if ( $this->fullAuth == TRUE ) {
+			// Check for required variables
+			if ( !empty($methodVars['eventId']) && !empty($methodVars['message']) ) {
+				$vars = array(
+					'method' => 'event.shout',
+					'api_key' => $this->auth->apiKey,
+					'event' => $methodVars['eventId'],
+					'message' => $methodVars['message'],
+					'sk' => $this->auth->sessionKey
+				);
+				$sig = $this->apiSig($this->auth->secret, $vars);
+				$vars['api_sig'] = $sig;
+				
+				if ( $call = $this->apiPostCall($vars) ) {
+					return TRUE;
+				}
+				else {
+					return FALSE;
+				}
+			}
+			else {
+				// Give a 91 error if incorrect variables are used
+				$this->handleError(91, 'You must include eventId and message variables in the call for this method');
+				return FALSE;
+			}
+		}
+		else {
+			// Give a 92 error if not fully authed
+			$this->handleError(92, 'Method requires full auth. Call auth.getSession using lastfmApiAuth class');
+			return FALSE;
+		}
+	}
 }
 
 ?>
