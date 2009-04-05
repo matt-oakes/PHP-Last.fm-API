63f231e25489e6d5c2f41d9b447c1f1d8c4627e4
Matt Oakes <oakesm9@googlemail.com>
2009-04-05 22:41:02 +0000

    Change some variable names

--- lastfmapi/api/geo.php
+++ lastfmapi/api/geo.php
@@ -59,40 +59,40 @@ class lastfmApiGeo extends lastfmApi {
 			}
 			
 			if ( $call = $this->apiGetCall($vars) ) {
-				$this->events['location'] = (string) $call->events['location'];
-				$this->events['currentPage'] = (string) $call->events['page'];
-				$this->events['totalPages'] = (string) $call->events['totalpages'];
-				$this->events['totalResults'] = (string) $call->events['total'];
+				$events['location'] = (string) $call->events['location'];
+				$events['currentPage'] = (string) $call->events['page'];
+				$events['totalPages'] = (string) $call->events['totalpages'];
+				$events['totalResults'] = (string) $call->events['total'];
 				$i = 0;
 				foreach ( $call->events->event as $event ) {
-					$this->events['events'][$i]['id'] = (string) $event->id;
-					$this->events['events'][$i]['title'] = (string) $event->title;
+					$events['events'][$i]['id'] = (string) $event->id;
+					$events['events'][$i]['title'] = (string) $event->title;
 					$ii = 0;
 					foreach ( $event->artists->artist as $artist ) {
-						$this->events['events'][$i]['artists'][$ii] = (string) $artist;
+						$events['events'][$i]['artists'][$ii] = (string) $artist;
 						$ii++;
 					}
-					$this->events['events'][$i]['headliner'] = (string) $event->artists->headliner;
-					$this->events['events'][$i]['venue']['name'] = (string) $event->venue->name;
-					$this->events['events'][$i]['venue']['location']['city'] = (string) $event->venue->location->city;
-					$this->events['events'][$i]['venue']['location']['country'] = (string) $event->venue->location->country;
-					$this->events['events'][$i]['venue']['location']['street'] = (string) $event->venue->location->street;
-					$this->events['events'][$i]['venue']['location']['postalcode'] = (string) $event->venue->location->postalcode;
+					$events['events'][$i]['headliner'] = (string) $event->artists->headliner;
+					$events['events'][$i]['venue']['name'] = (string) $event->venue->name;
+					$events['events'][$i]['venue']['location']['city'] = (string) $event->venue->location->city;
+					$events['events'][$i]['venue']['location']['country'] = (string) $event->venue->location->country;
+					$events['events'][$i]['venue']['location']['street'] = (string) $event->venue->location->street;
+					$events['events'][$i]['venue']['location']['postalcode'] = (string) $event->venue->location->postalcode;
 					$geoTags = $event->venue->location->children('http://www.w3.org/2003/01/geo/wgs84_pos#');
-					$this->events['events'][$i]['venue']['location']['point']['lat'] = (string) $geoTags->point->lat;
-					$this->events['events'][$i]['venue']['location']['point']['long'] = (string) $geoTags->point->long;
-					$this->events['events'][$i]['venue']['location']['timezone'] = (string) $event->venue->location->timezone;
-					$this->events['events'][$i]['venue']['url'] = (string) $event->venue->url;
-					$this->events['events'][$i]['startDate'] = strtotime(trim((string) $event->startDate));
-					$this->events['events'][$i]['startTime'] = (string) $event->startTime;
-					$this->events['events'][$i]['description'] = (string) $event->description;
-					$this->events['events'][$i]['attendance'] = (string) $event->attendance;
-					$this->events['events'][$i]['reviews'] = (string) $event->reviews;
-					$this->events['events'][$i]['tag'] = (string) $event->tag;
-					$this->events['events'][$i]['url'] = (string) $event->url;
+					$events['events'][$i]['venue']['location']['point']['lat'] = (string) $geoTags->point->lat;
+					$events['events'][$i]['venue']['location']['point']['long'] = (string) $geoTags->point->long;
+					$events['events'][$i]['venue']['location']['timezone'] = (string) $event->venue->location->timezone;
+					$events['events'][$i]['venue']['url'] = (string) $event->venue->url;
+					$events['events'][$i]['startDate'] = strtotime(trim((string) $event->startDate));
+					$events['events'][$i]['startTime'] = (string) $event->startTime;
+					$events['events'][$i]['description'] = (string) $event->description;
+					$events['events'][$i]['attendance'] = (string) $event->attendance;
+					$events['events'][$i]['reviews'] = (string) $event->reviews;
+					$events['events'][$i]['tag'] = (string) $event->tag;
+					$events['events'][$i]['url'] = (string) $event->url;
 					$i++;
 				}
-				return $this->events;
+				return $events;
 			}
 			else {
 				return FALSE;
@@ -122,19 +122,19 @@ class lastfmApiGeo extends lastfmApi {
 			if ( $call = $this->apiGetCall($vars) ) {
 				$i = 0;
 				foreach ( $call->topartists->artist as $artist ) {
-					$this->artists[$i]['name'] = (string) $artist->name;
-					$this->artists[$i]['rank'] = (string) $artist['rank'];
-					$this->artists[$i]['playcount'] = (string) $artist->playcount;
-					$this->artists[$i]['mbid'] = (string) $artist->mbid;
-					$this->artists[$i]['url'] = (string) $artist->url;
-					$this->artists[$i]['streamable'] = (string) $artist->streamable;
-					$this->artists[$i]['image']['small'] = (string) $artist->image[0];
-					$this->artists[$i]['image']['medium'] = (string) $artist->image[1];
-					$this->artists[$i]['image']['large'] = (string) $artist->image[2];
+					$topArtists[$i]['name'] = (string) $artist->name;
+					$topArtists[$i]['rank'] = (string) $artist['rank'];
+					$topArtists[$i]['playcount'] = (string) $artist->playcount;
+					$topArtists[$i]['mbid'] = (string) $artist->mbid;
+					$topArtists[$i]['url'] = (string) $artist->url;
+					$topArtists[$i]['streamable'] = (string) $artist->streamable;
+					$topArtists[$i]['image']['small'] = (string) $artist->image[0];
+					$topArtists[$i]['image']['medium'] = (string) $artist->image[1];
+					$topArtists[$i]['image']['large'] = (string) $artist->image[2];
 					$i++;
 				}
 				
-				return $this->artists;
+				return $topArtists;
 			}
 			else {
 				return FALSE;
@@ -164,23 +164,23 @@ class lastfmApiGeo extends lastfmApi {
 			if ( $call = $this->apiGetCall($vars) ) {
 				$i = 0;
 				foreach ( $call->toptracks->track as $track ) {
-					$this->tracks[$i]['name'] = (string) $track->name;
-					$this->tracks[$i]['rank'] = (string) $track['rank'];
-					$this->tracks[$i]['playcount'] = (string) $track->playcount;
-					$this->tracks[$i]['mbid'] = (string) $track->mbid;
-					$this->tracks[$i]['url'] = (string) $track->url;
-					$this->tracks[$i]['streamable'] = (string) $track->streamable;
-					$this->tracks[$i]['fulltrack'] = (string) $track->streamable['fulltrack'];
-					$this->tracks[$i]['artist']['name'] = (string) $track->artist->name;
-					$this->tracks[$i]['artist']['mbid'] = (string) $track->artist->mbid;
-					$this->tracks[$i]['artist']['url'] = (string) $track->artist->url;
-					$this->tracks[$i]['image']['small'] = (string) $track->image[0];
-					$this->tracks[$i]['image']['medium'] = (string) $track->image[1];
-					$this->tracks[$i]['image']['large'] = (string) $track->image[2];
+					$topTracks[$i]['name'] = (string) $track->name;
+					$topTracks[$i]['rank'] = (string) $track['rank'];
+					$topTracks[$i]['playcount'] = (string) $track->playcount;
+					$topTracks[$i]['mbid'] = (string) $track->mbid;
+					$topTracks[$i]['url'] = (string) $track->url;
+					$topTracks[$i]['streamable'] = (string) $track->streamable;
+					$topTracks[$i]['fulltrack'] = (string) $track->streamable['fulltrack'];
+					$topTracks[$i]['artist']['name'] = (string) $track->artist->name;
+					$topTracks[$i]['artist']['mbid'] = (string) $track->artist->mbid;
+					$topTracks[$i]['artist']['url'] = (string) $track->artist->url;
+					$topTracks[$i]['image']['small'] = (string) $track->image[0];
+					$topTracks[$i]['image']['medium'] = (string) $track->image[1];
+					$topTracks[$i]['image']['large'] = (string) $track->image[2];
 					$i++;
 				}
 				
-				return $this->tracks;
+				return $topTracks;
 			}
 			else {
 				return FALSE;
