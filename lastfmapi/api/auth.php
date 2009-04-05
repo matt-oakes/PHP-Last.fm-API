6044d8c9fb56f0d65a626cbffb1cce29f26693cb
Matt Oakes <oakesm9@googlemail.com>
2009-04-05 22:21:25 +0000

    Added phpdoc comments

--- lastfmapi/api/auth.php
+++ lastfmapi/api/auth.php
@@ -1,15 +1,59 @@
 <?php
-
+/**
+ * File that stores api calls for getting authentication values
+ * @package apicalls
+ */
+/**
+ * Allows access to the api requests relating to authentication
+ * @package apicalls
+ */
 class lastfmApiAuth extends lastfmApi {
+	/**
+	 * Stores the api key
+	 * @access public
+	 * @var string
+	 */
 	public $apiKey;
+	/**
+	 * Stores the secret
+	 * @access public
+	 * @var string
+	 */
 	public $secret;
+	/**
+	 * Stores the authenticated username
+	 * @access public
+	 * @var string
+	 */
 	public $username;
+	/**
+	 * Stores the session key
+	 * @access public
+	 * @var string
+	 */
 	public $sessionKey;
+	/**
+	 * Stores the users subscriber status
+	 * @access public
+	 * @var boolean
+	 */
 	public $subscriber;
 	
+	/**
+	 * Stores the authentication token
+	 * @access private
+	 * @var string
+	 */
 	private $token;
 	
-	function __construct($method, $vars) {
+	/**
+	 * Run when the class is created
+	 * @param string $method <i>getsession</i> to get a new session, <i>setsession</i> to set a current session
+	 * @param array $vars An array of variables to pass to the class
+	 * @return void
+	 * @access public
+	 */
+	public function __construct($method, $vars) {
 		if ( $method == 'getsession' ) {
 			if ( !empty($vars['apiKey']) && !empty($vars['secret']) && !empty($vars['token']) ) {
 				$this->apiKey = $vars['apiKey'];
@@ -43,6 +87,11 @@ class lastfmApiAuth extends lastfmApi {
 		}
 	}
 	
+	/**
+	 * Internal method uses to get a new session via an api call
+	 * @return void
+	 * @access private
+	 */
 	private function getSession() {
 		$vars = array(
 			'method' => 'auth.getsession',
